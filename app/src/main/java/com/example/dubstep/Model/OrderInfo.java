6066471321c203bef8ff518e49ccd0e54afc4227
Adityesh <adityeshmishra98@gmail.com>
2021-01-02 13:46:33 -0500

    Order place to database

--- app/src/main/java/com/example/dubstep/Model/OrderInfo.java
+++ app/src/main/java/com/example/dubstep/Model/OrderInfo.java
@@ -1,7 +1,36 @@
 package com.example.dubstep.Model;
 
+import java.util.List;
+
 public class OrderInfo {
-    CartItem[] cartItems;
-    int status;
-    CartInfo cartInfo;
+
+    private String date;
+    private List<Object> items;
+    private CartInfo cartInfo;
+    private String userId;
+    public OrderInfo() {
+    }
+
+    public OrderInfo(String date, List<Object> items, CartInfo cartInfo, String userId) {
+        this.date = date;
+        this.items = items;
+        this.cartInfo = cartInfo;
+        this.userId = userId;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public List<Object> getItems() {
+        return items;
+    }
+
+    public CartInfo getCartInfo() {
+        return cartInfo;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
 }
